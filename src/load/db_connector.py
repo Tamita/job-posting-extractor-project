f5f959636962d39873039f78536cb0d2acb4bc17
Tati <tamita@Mac.lan>
2026-05-12 12:44:47 -0500

    feat(load): add PostgreSQL engine connector

---
+++ src/load/db_connector.py
@@ -0,0 +1,37 @@
+import logging
+from urllib.parse import quote_plus
+
+from sqlalchemy import Engine, create_engine
+
+from src.config.settings import (
+    POSTGRES_DB,
+    POSTGRES_HOST,
+    POSTGRES_PASSWORD,
+    POSTGRES_PORT,
+    POSTGRES_USER,
+)
+
+logger = logging.getLogger(__name__)
+
+
+def build_postgres_connection_url() -> str:
+    encoded_password = quote_plus(POSTGRES_PASSWORD)
+
+    return (
+        f"postgresql+psycopg://{POSTGRES_USER}:{encoded_password}"
+        f"@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+    )
+
+
+def create_postgres_engine() -> Engine:
+    connection_url = build_postgres_connection_url()
+
+    logger.info("Creating PostgreSQL engine for database '%s'", POSTGRES_DB)
+
+    try:
+        engine = create_engine(connection_url)
+        logger.info("PostgreSQL engine created successfully")
+        return engine
+    except Exception:
+        logger.exception("Failed to create PostgreSQL engine")
+        raise
--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ import logging
 from .utils.logging_config import setup_logging
 from .extract.data_extractor import read_raw_jobs_csv
 from .transform.data_transformer import parse_semi_structured_columns
-from .transform.raw_jobs_schema import validate_raw_jobs_dataframe
+from .load.db_connector import build_postgres_connection_url, create_postgres_engine
 
 logger = logging.getLogger(__name__)
 
@@ -17,14 +17,18 @@ def main() -> None:
 
         parsed_df = parse_semi_structured_columns(df)
 
-        validated_df = validate_raw_jobs_dataframe(parsed_df)
+        # validated_df = validate_raw_jobs_dataframe(parsed_df)
 
-        if validated_df.empty:
+        print(build_postgres_connection_url())
+        engine = create_postgres_engine()
+        print(engine)
+
+        if parsed_df.empty:
             logger.warning("Application completed with an empty dataset")
         else:
             logger.info(
                 "Application execution finished successfully with %s records",
-                len(validated_df),
+                len(parsed_df),
             )
     except Exception:
         logger.exception("Application execution failed")
