eedaa96b3814b115f6020c41cb6aeb69558d1748
Tati <tamita@Mac.lan>
2026-05-13 21:25:42 -0500

    refactor(db): simplify normalized schema for jobs and skills model

--- src/load/sql/normalized_seed_jobs.sql
+++ src/load/sql/normalized_seed_jobs.sql
@@ -1,4 +1,5 @@
 INSERT INTO jobs (
+    raw_job_id,
     job_title_id,
     company_id,
     job_location_id,
@@ -14,43 +15,40 @@ INSERT INTO jobs (
     job_no_degree_mention,
     job_health_insurance
 )
-SELECT DISTINCT
-    jt.job_title_id,
-    c.company_id,
-    jl.location_id AS job_location_id,
-    sl.location_id AS search_location_id,
-    p.platform_id,
-    st.schedule_type_id,
-    sr.salary_rate_id,
-    r.job_title,
-    job_posted_date,
-    COALESCE(r.job_work_from_home, FALSE),
-    r.salary_year_avg,
-    r.salary_hour_avg,
-    COALESCE(r.job_no_degree_mention, FALSE),
-    COALESCE(r.job_health_insurance, FALSE)
-FROM public.raw_jobs AS r
-LEFT JOIN job_titles AS jt
-    ON jt.job_title_name = NULLIF(BTRIM(r.job_title_short), '')
-LEFT JOIN companies AS c
-    ON c.company_name = NULLIF(
-        BTRIM(
-            TRANSLATE(r.company_name, CHR(34), '')
-        ),
-        ''
-    )
-LEFT JOIN locations AS jl
-    ON jl.raw_location = NULLIF(BTRIM(r.job_location), '')
-LEFT JOIN locations AS sl
-    ON sl.raw_location = NULLIF(BTRIM(r.search_location), '')
-LEFT JOIN platforms AS p
-    ON p.platform_name = CASE
-        WHEN lower(BTRIM(r.job_via)) LIKE 'via %'
-            THEN NULLIF(BTRIM(SUBSTRING(BTRIM(r.job_via) FROM 5)), '')
-        ELSE NULLIF(BTRIM(r.job_via), '')
+SELECT
+    raw_jobs.raw_job_id,
+    job_titles.job_title_id,
+    companies.company_id,
+    job_location.location_id AS job_location_id,
+    search_location.location_id AS search_location_id,
+    platforms.platform_id,
+    schedule_types.schedule_type_id,
+    salary_rates.salary_rate_id,
+    NULLIF(BTRIM(raw_jobs.job_title), '') AS job_title,
+    raw_jobs.job_posted_date,
+    COALESCE(raw_jobs.job_work_from_home, false),
+    raw_jobs.salary_year_avg,
+    raw_jobs.salary_hour_avg,
+    COALESCE(raw_jobs.job_no_degree_mention, false),
+    COALESCE(raw_jobs.job_health_insurance, false)
+FROM public.raw_jobs AS raw_jobs
+LEFT JOIN job_titles
+    ON job_titles.job_title_name = NULLIF(BTRIM(raw_jobs.job_title_short), '')
+LEFT JOIN companies
+    ON companies.company_name = NULLIF(BTRIM(raw_jobs.company_name), '')
+LEFT JOIN locations AS job_location
+    ON job_location.raw_location = NULLIF(BTRIM(raw_jobs.job_location), '')
+LEFT JOIN locations AS search_location
+    ON search_location.raw_location = NULLIF(BTRIM(raw_jobs.search_location), '')
+LEFT JOIN platforms
+    ON platforms.platform_name = CASE
+        WHEN lower(BTRIM(raw_jobs.job_via)) LIKE 'via %'
+            THEN NULLIF(BTRIM(SUBSTRING(BTRIM(raw_jobs.job_via) FROM 5)), '')
+        ELSE NULLIF(BTRIM(raw_jobs.job_via), '')
     END
-LEFT JOIN schedule_types AS st
-    ON st.schedule_type_name = NULLIF(BTRIM(r.job_schedule_type), '')
-LEFT JOIN salary_rates AS sr
-    ON sr.salary_rate_name = lower(NULLIF(BTRIM(r.salary_rate), ''))
-WHERE NULLIF(BTRIM(r.job_title), '') IS NOT NULL;
+LEFT JOIN schedule_types
+    ON schedule_types.schedule_type_name = NULLIF(BTRIM(raw_jobs.job_schedule_type), '')
+LEFT JOIN salary_rates
+    ON salary_rates.salary_rate_name = lower(NULLIF(BTRIM(raw_jobs.salary_rate), ''))
+WHERE NULLIF(BTRIM(raw_jobs.job_title), '') IS NOT NULL
+ON CONFLICT (raw_job_id) DO NOTHING;
